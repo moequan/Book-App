83941c29ec2f1478c56b5c52cbc0f3daaa9c6546
moequan <maurice.s93@web.de>
2020-01-16 13:02:24 +0100

    Removing files in repooooo

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+       Heeeeelo
+     </h1>
+    </div>
+  );
+}
+
+export default App;
